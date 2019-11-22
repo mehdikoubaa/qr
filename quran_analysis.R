390b1a4e05d0d84ee604b9608975243c343a034e
mehdikoubaa <koubaa.mehdi@gmail.com>
2019-11-22 14:52:46 +0100

    update R script for today changes

--- quran_analysis.R
+++ quran_analysis.R
@@ -12,6 +12,7 @@ qr <- quran_ar_min %>% select(surah_id,ayah_id,surah_title_en,ayah =text)
 qr_k <-  quran_ar_min %>% 
   select(surah_id,ayah_id,surah_title_en,ayah =text) %>%
   unnest_tokens(output=kalima,input=ayah,token="words")
+write_csv(qr_k,"quran_by_words")
 
 #get occurence of words
 qr_occ <-  quran_ar_min %>% 
@@ -21,6 +22,16 @@ qr_occ <-  quran_ar_min %>%
   summarise(marrat=n()) %>% 
   arrange (desc(marrat))
 save(qr_occ,file="quran_occurences.rda")
+
+pl_occ <- qr_occ %>% slice(1:20)%>%
+  ggplot(aes(fct_rev(fct_inorder(kalima)),marrat))+ 
+  #forcats fct to order factors
+  geom_col(fill="red") +
+  coord_flip()+
+  labs(y="عدد المرات", x="الكلمة في القرآن",title="تكرر الكلمات في القرآن دون حذف أي كلمة")
+pl_occ
+ggsave("redondance_words.png",plot=pl_occ)
+
 
 #remove words in "stop words"
 #____1/ with stopwords::
@@ -36,7 +47,16 @@ qr_occ_cln1 <- qr_occ %>%
 library(arabicStemR)  # Has a list of Arabic stopwords
 arabic_stopwords <- tibble(word = removeStopWords("سلام")$arabicStopwordList)
 qr_occ_cln2 <- qr_occ %>% 
-  filter (!kalima %in% arabic_stopwords$word) 
+  filter (!kalima %in% arabic_stopwords$word)
+#plot 20 first occurences
+pl_occ_cln2 <- qr_occ_cln2 %>% slice(1:20)%>%
+  ggplot(aes(fct_rev(fct_inorder(kalima)),marrat))+ 
+  #forcats fct to order factors
+  geom_col(fill="blue") +
+  coord_flip()+
+  labs(y="عدد المرات", x="الكلمة في القرآن",title="تكرار الكلمات في القرآن بعد حذف كلمات الوقوف")
+pl_occ_cln2
+ggsave("redondance_without_stpwords.png",plot=pl_occ_cln2)
 
 #see all words starting with "waw"
 waw <- qr_occ_cln2[str_detect(qr_occ_cln2$kalima,"^و.*"),]
